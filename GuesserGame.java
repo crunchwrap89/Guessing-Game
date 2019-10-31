97c9e207c90840ce2e6929218b095a5da68b78dd
Crunchwrap89 <marcus89n@gmail.com>
2019-10-31 14:26:35 +0100

    Task 5 inlagd och committar detta

--- GuesserGame.java
+++ GuesserGame.java
@@ -1,8 +1,11 @@
 public class GuesserGame {
 
 	public static void main(String[] args) {
+
+	int start = Integer.parseInt(args[0]);
+	int stop = Integer.parseInt(args[1]);
 		
-		 Guesser guesser = new Guesser(0, 1000);
+		 Guesser guesser = new Guesser(start, stop);
 		 guesser.start();
 		 
 		 
